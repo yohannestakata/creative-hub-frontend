7a14bc769fe13fac0ca31ac1ef646b7bddbb1314
yohannestakata <yohannestakata1@gmail.com>
2025-01-14 06:04:26 +0300

    fix mobile typography

--- src/components/pages/insight/article/ArticlePresentation.tsx
+++ src/components/pages/insight/article/ArticlePresentation.tsx
@@ -14,7 +14,7 @@ interface ArticlePresentationProps {
 const ArticlePresentation = ({ post }: ArticlePresentationProps) => {
   return (
     <>
-      <header className="relative grid min-h-[50vh] grid-cols-12 items-center px-8 py-16 md:gap-0 lg:gap-4">
+      <header className="relative grid grid-cols-12 items-center gap-4 px-4 py-16 lg:px-8">
         <div className="absolute inset-0">
           <Image
             src={post.headerImage}
@@ -27,10 +27,10 @@ const ArticlePresentation = ({ post }: ArticlePresentationProps) => {
           <div className="absolute inset-0 bg-gradient-to-b from-black/80 to-black/60" />
         </div>
 
-        <h1 className="z-10 col-span-12 font-display text-lg font-medium leading-[2rem] tracking-tighter text-white md:col-span-10 md:col-start-2 md:text-xl md:leading-[2.5rem] lg:col-span-8 lg:col-start-3 lg:text-2xl lg:leading-none">
+        <h1 className="z-10 col-span-12 font-display text-xl font-medium leading-[110%] tracking-tighter text-white md:col-span-10 md:col-start-2 md:text-xl md:leading-[2.5rem] lg:col-span-8 lg:col-start-3 lg:text-2xl lg:leading-none">
           {post.title}
         </h1>
-        <div className="z-10 col-span-12 flex items-center gap-3 md:col-span-10 md:col-start-2 lg:col-span-8 lg:col-start-3">
+        <div className="z-10 col-span-12 flex items-center gap-2 md:col-span-10 md:col-start-2 lg:col-span-8 lg:col-start-3">
           <div className="aspect-square w-10 overflow-hidden rounded-full md:w-12">
             <Image
               src={post.authorAvatar}
@@ -43,7 +43,7 @@ const ArticlePresentation = ({ post }: ArticlePresentationProps) => {
           <span className="text-background">{post.author}</span>
         </div>
       </header>
-      <main className="grid grid-cols-12 gap-4 px-8 py-12">
+      <main className="grid grid-cols-12 gap-4 px-4 py-8 lg:px-8 lg:py-12">
         <div className="col-span-12 md:col-span-10 md:col-start-2 lg:col-span-8 lg:col-start-3">
           <RichText content={post.content} />
         </div>
