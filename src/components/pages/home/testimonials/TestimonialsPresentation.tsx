e9fc98fb0de2160b0cbfd106188fc430569124e7
yohannestakata <yohannestakata1@gmail.com>
2025-01-30 21:48:04 +0300

    register button on event cards

--- src/components/pages/home/testimonials/TestimonialsPresentation.tsx
+++ src/components/pages/home/testimonials/TestimonialsPresentation.tsx
@@ -9,7 +9,6 @@ import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import { useCallback } from "react";
 import Image from "next/image";
-import { LucideQuote, Quote, TextQuote } from "lucide-react";
 
 interface Testimonial {
   id: string;
